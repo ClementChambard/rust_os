d55e91f5e684e6889f77063070bf14185c7ab2f1
ClementChambard <cclems2002@gmail.com>
2023-06-07 16:42:36 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    loop {}
+}
